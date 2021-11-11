52962eee37800d363bd880ae4239a172a707c0df
Donald Wu <yeukfei02@gmail.com>
2021-11-11 11:21:09 +0800

    fix: 🐛 fix helpers getRootUrl

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,14 @@
 export const getRootUrl = (): string => {
-  const rootUrl = `https://divisionly-api.herokuapp.com/api`;
+  let rootUrl = "";
+
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    rootUrl = `localhost:3000/api`;
+  } else {
+    rootUrl = `https://divisionly-api.herokuapp.com/api`;
+  }
+
   return rootUrl;
 };
